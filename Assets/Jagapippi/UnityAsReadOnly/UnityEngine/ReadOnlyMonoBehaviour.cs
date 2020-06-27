745fdb244c6083be5f619df7082cdef20ee59dc6
su10 <1687366+su10@users.noreply.github.com>
2020-06-28 00:55:08 +0900

    ReadOnlyMonoBehaviourクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyMonoBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyMonoBehaviour : IReadOnlyBehaviour
+    {
+#if UNITY_EDITOR
+        bool runInEditMode { get; }
+#endif
+        bool useGUILayout { get; }
+    }
+
+    public class ReadOnlyMonoBehaviour : ReadOnlyBehaviour<MonoBehaviour>, IReadOnlyMonoBehaviour
+    {
+        public ReadOnlyMonoBehaviour(MonoBehaviour obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+#if UNITY_EDITOR
+        public bool runInEditMode => _obj.runInEditMode;
+#endif
+        public bool useGUILayout => _obj.useGUILayout;
+
+        #endregion
+
+        #region Public Methods
+
+        // CancelInvoke
+        // Invoke
+        // InvokeRepeating
+        // IsInvoking
+        // StartCoroutine
+        // StopAllCoroutines
+        // StopCoroutine
+
+        #endregion
+    }
+
+    public static class MonoBehaviourExtensions
+    {
+        public static ReadOnlyMonoBehaviour AsReadOnly(this MonoBehaviour self) => new ReadOnlyMonoBehaviour(self);
+    }
+}
