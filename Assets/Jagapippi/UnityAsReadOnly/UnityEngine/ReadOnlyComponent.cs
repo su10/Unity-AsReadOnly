c7c5724e465b95b1fc0ea8eec3e7784e52bcc85a
su10 <1687366+su10@users.noreply.github.com>
2020-06-28 00:44:17 +0900

    ReadOnlyComponentクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyComponent.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyComponent : IReadOnlyObject
+    {
+        ReadOnlyGameObject gameObject { get; }
+        string tag { get; }
+        ReadOnlyTransform transform { get; }
+        bool CompareTag(string tag);
+    }
+
+    public class ReadOnlyComponent<T> : ReadOnlyObject<T>, IReadOnlyComponent where T : Component
+    {
+        protected ReadOnlyComponent(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public ReadOnlyGameObject gameObject => _obj.gameObject.AsReadOnly();
+        public string tag => _obj.tag;
+        public ReadOnlyTransform transform => _obj.transform.AsReadOnly();
+
+        #endregion
+
+        #region Methods
+
+        // BroadcastMessage
+        public bool CompareTag(string tag) => _obj.CompareTag(tag);
+        // GetComponent
+        // GetComponentInChildren
+        // GetComponentInParent
+        // GetComponents
+        // GetComponentsInChildren
+        // GetComponentsInParent
+        // SendMessage
+        // SendMessageUpwards
+
+        #endregion
+    }
+
+    public class ReadOnlyComponent : ReadOnlyComponent<Component>
+    {
+        public ReadOnlyComponent(Component obj) : base(obj)
+        {
+        }
+    }
+
+    public static class ComponentExtensions
+    {
+        public static ReadOnlyComponent AsReadOnly(this Component self) => new ReadOnlyComponent(self);
+    }
+}
