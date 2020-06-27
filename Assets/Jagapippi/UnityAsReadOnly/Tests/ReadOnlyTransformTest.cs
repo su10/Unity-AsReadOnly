48399f7e1459dfc161d376829474fb58818f47bd
su10 <1687366+su10@users.noreply.github.com>
2020-06-28 00:45:48 +0900

    ReadOnlyTransformクラスとそのテスト追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/Tests/ReadOnlyTransformTest.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Jagapippi.UnityAsReadOnly.Tests
+{
+    public class ReadOnlyTransformTest
+    {
+        private Transform _transform;
+
+        [SetUp]
+        public void SetUp()
+        {
+            if (_transform) Object.Destroy(_transform.gameObject);
+            _transform = new GameObject("ReadOnlyTransformTest").transform;
+        }
+
+        [Test]
+        public void TransformEqualsNull()
+        {
+            Transform transform = null;
+
+            Assert.IsFalse(transform);
+            Assert.IsTrue(transform == null);
+            Assert.IsFalse(transform != null);
+            Assert.IsTrue(ReferenceEquals(transform, null));
+
+            transform = _transform;
+
+            Assert.IsTrue(transform);
+            Assert.IsFalse(transform == null);
+            Assert.IsTrue(transform != null);
+            Assert.IsFalse(transform.Equals(null));
+            Assert.IsFalse(ReferenceEquals(transform, null));
+        }
+
+        [Test]
+        public void TransformEqualsOther()
+        {
+            var transform1 = new GameObject("LHS").transform;
+            var transform2 = new GameObject("RHS").transform;
+
+            Assert.IsFalse(transform1 == transform2);
+            Assert.IsTrue(transform1 != transform2);
+            Assert.IsFalse(transform1.Equals(transform2));
+            Assert.IsFalse(ReferenceEquals(transform1, transform2));
+        }
+
+        [Test]
+        public void TransformEqualsSame()
+        {
+            var transform1 = _transform;
+            var transform2 = transform1;
+
+            Assert.IsTrue(transform1 == transform2);
+            Assert.IsFalse(transform1 != transform2);
+            Assert.IsTrue(transform1.Equals(transform2));
+            Assert.IsTrue(ReferenceEquals(transform1, transform2));
+        }
+
+        [UnityTest]
+        public IEnumerator TransformDestroy()
+        {
+            Object.Destroy(_transform.gameObject);
+
+            Assert.IsTrue(_transform);
+            Assert.IsFalse(_transform == null);
+            Assert.IsTrue(_transform != null);
+            Assert.IsFalse(_transform.Equals(null));
+
+            yield return null;
+
+            Assert.IsFalse(_transform);
+            Assert.IsTrue(_transform == null);
+            Assert.IsFalse(_transform != null);
+            Assert.IsTrue(_transform.Equals(null));
+        }
+
+        [Test]
+        public void ReadOnlyTransformEqualsNull()
+        {
+            ReadOnlyTransform transform = null;
+
+            Assert.IsFalse(transform);
+            Assert.IsTrue(transform == null);
+            Assert.IsFalse(transform != null);
+            Assert.IsTrue(ReferenceEquals(transform, null));
+
+            transform = _transform.AsReadOnly();
+
+            Assert.IsTrue(transform);
+            Assert.IsFalse(transform == null);
+            Assert.IsTrue(transform != null);
+            Assert.IsFalse(transform.Equals(null));
+            Assert.IsFalse(ReferenceEquals(transform, null));
+        }
+
+        [Test]
+        public void ReadOnlyTransformEqualsOther()
+        {
+            var go1 = new GameObject("LHS");
+            var go2 = new GameObject("RHS");
+            var transform1 = go1.transform.AsReadOnly();
+            var transform2 = go2.transform.AsReadOnly();
+
+            Assert.IsFalse(transform1 == transform2);
+            Assert.IsTrue(transform1 != transform2);
+            Assert.IsFalse(transform1.Equals(transform2));
+            Assert.IsFalse(ReferenceEquals(transform1, transform2));
+
+            Object.Destroy(go1);
+            Object.Destroy(go2);
+        }
+
+        [Test]
+        public void ReadOnlyTransformEqualsSame()
+        {
+            var transform1 = _transform.AsReadOnly();
+
+            Assert.IsTrue(transform1 == _transform);
+            Assert.IsFalse(transform1 != _transform);
+            Assert.IsTrue(transform1.Equals(_transform));
+            Assert.IsFalse(ReferenceEquals(transform1, _transform));
+
+            var transform2 = transform1;
+
+            Assert.IsTrue(transform1 == transform2);
+            Assert.IsFalse(transform1 != transform2);
+            Assert.IsTrue(transform1.Equals(transform2));
+            Assert.IsTrue(ReferenceEquals(transform1, transform2));
+
+            transform2 = _transform.AsReadOnly();
+            Assert.IsTrue(transform1 == transform2);
+            Assert.IsFalse(transform1 != transform2);
+            Assert.IsTrue(transform1.Equals(transform2));
+            Assert.IsFalse(ReferenceEquals(transform1, transform2));
+        }
+
+        [UnityTest]
+        public IEnumerator ReadOnlyTransformDestroy()
+        {
+            var transform = _transform.AsReadOnly();
+            Object.Destroy(_transform.gameObject);
+
+            Assert.IsTrue(transform);
+            Assert.IsFalse(transform == null);
+            Assert.IsTrue(transform != null);
+            Assert.IsFalse(transform.Equals(null));
+
+            yield return null;
+
+            Assert.IsFalse(transform);
+            Assert.IsTrue(transform == null);
+            Assert.IsFalse(transform != null);
+            Assert.IsTrue(transform.Equals(null));
+        }
+    }
+}
---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyTransform.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public class ReadOnlyTransform : ReadOnlyComponent<Transform>
+    {
+        public ReadOnlyTransform(Transform obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public int childCount => _obj.childCount;
+        public Vector3 eulerAngles => _obj.eulerAngles;
+        public Vector3 forward => _obj.forward;
+        public bool hasChanged => _obj.hasChanged;
+        public int hierarchyCapacity => _obj.hierarchyCapacity;
+        public int hierarchyCount => _obj.hierarchyCount;
+        public Vector3 localEulerAngles => _obj.localEulerAngles;
+        public Vector3 localPosition => _obj.localPosition;
+        public Quaternion localRotation => _obj.localRotation;
+        public Vector3 localScale => _obj.localScale;
+        public Matrix4x4 localToWorldMatrix => _obj.localToWorldMatrix;
+        public Vector3 lossyScale => _obj.lossyScale;
+        public ReadOnlyTransform parent => _obj.parent.AsReadOnly();
+        public Vector3 position => _obj.position;
+        public Vector3 right => _obj.right;
+        public ReadOnlyTransform root => _obj.root.AsReadOnly();
+        public Quaternion rotation => _obj.rotation;
+        public Vector3 up => _obj.up;
+        public Matrix4x4 worldToLocalMatrix => _obj.worldToLocalMatrix;
+
+        #endregion
+
+        #region Public Methods
+
+        // DetachChildren
+        public ReadOnlyTransform Find(string n) => _obj.Find(n).AsReadOnly();
+        public ReadOnlyTransform GetChild(int index) => _obj.GetChild(index).AsReadOnly();
+        public int GetSiblingIndex() => _obj.GetSiblingIndex();
+        public Vector3 InverseTransformDirection(Vector3 direction) => _obj.InverseTransformDirection(direction);
+        public Vector3 InverseTransformDirection(float x, float y, float z) => _obj.InverseTransformDirection(x, y, z);
+        public Vector3 InverseTransformPoint(Vector3 position) => _obj.InverseTransformPoint(position);
+        public Vector3 InverseTransformPoint(float x, float y, float z) => _obj.InverseTransformPoint(x, y, z);
+        public Vector3 InverseTransformVector(Vector3 vector) => _obj.InverseTransformVector(vector);
+        public Vector3 InverseTransformVector(float x, float y, float z) => _obj.InverseTransformVector(x, y, z);
+        public bool IsChildOf(Transform parent) => _obj.IsChildOf(parent);
+        public bool IsChildOf(ReadOnlyTransform parent) => _obj.IsChildOf(parent._obj);
+        // LookAt
+        // Rotate
+        // RotateAround
+        // SetAsFirstSibling
+        // SetAsLastSibling
+        // SetParent
+        // SetPositionAndRotation
+        // SetSiblingIndex
+        public Vector3 TransformDirection(Vector3 direction) => _obj.TransformDirection(direction);
+        public Vector3 TransformDirection(float x, float y, float z) => _obj.TransformDirection(x, y, z);
+        public Vector3 TransformPoint(Vector3 position) => _obj.TransformPoint(position);
+        public Vector3 TransformPoint(float x, float y, float z) => _obj.TransformPoint(x, y, z);
+        public Vector3 TransformVector(Vector3 vector) => _obj.TransformVector(vector);
+        public Vector3 TransformVector(float x, float y, float z) => _obj.TransformVector(x, y, z);
+        // Translate
+
+        #endregion
+    }
+
+    public static class TransformExtensions
+    {
+        public static ReadOnlyTransform AsReadOnly(this Transform self) => new ReadOnlyTransform(self);
+    }
+}
