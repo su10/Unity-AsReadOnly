f512567635320a6b9ccdb462efd3be0dbf32419f
su10 <1687366+su10@users.noreply.github.com>
2020-06-28 00:44:03 +0900

    ReadOnlyObjectクラスとそのテスト追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/Tests/ReadOnlyObjectTest.cs
@@ -0,0 +1,110 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Jagapippi.UnityAsReadOnly.Tests
+{
+    public class ReadOnlyObjectTest
+    {
+        [Test]
+        public void ObjectEqualsNull()
+        {
+            Object obj = null;
+
+            Assert.IsFalse(obj);
+            Assert.IsTrue(obj == null);
+            Assert.IsFalse(obj != null);
+            Assert.IsTrue(ReferenceEquals(obj, null));
+
+            obj = new Object();
+
+            // NOTE: This is Unity's intended implementation..
+            Assert.IsFalse(obj);
+            Assert.IsTrue(obj == null);
+            Assert.IsFalse(obj != null);
+            Assert.IsTrue(obj.Equals(null));
+            Assert.IsFalse(ReferenceEquals(obj, null));
+        }
+
+        [Test]
+        public void ObjectEqualsOther()
+        {
+            var obj1 = new Object();
+            var obj2 = new Object();
+
+            Assert.IsTrue(obj1 == obj2);
+            Assert.IsFalse(obj1 != obj2);
+            Assert.IsTrue(obj1.Equals(obj2));
+            Assert.IsFalse(ReferenceEquals(obj1, obj2));
+        }
+
+        [Test]
+        public void ObjectEqualsSame()
+        {
+            var obj1 = new Object();
+            var obj2 = obj1;
+
+            Assert.IsTrue(obj1 == obj2);
+            Assert.IsFalse(obj1 != obj2);
+            Assert.IsTrue(obj1.Equals(obj2));
+            Assert.IsTrue(ReferenceEquals(obj1, obj2));
+        }
+
+        [Test]
+        public void ReadOnlyObjectEqualsNull()
+        {
+            ReadOnlyObject obj = null;
+
+            Assert.IsFalse(obj);
+            Assert.IsTrue(obj == null);
+            Assert.IsFalse(obj != null);
+            Assert.IsTrue(ReferenceEquals(obj, null));
+
+            obj = new ReadOnlyObject(new Object());
+
+            // NOTE: This is Unity's intended implementation..
+            Assert.IsFalse(obj);
+            Assert.IsTrue(obj == null);
+            Assert.IsFalse(obj != null);
+            Assert.IsTrue(obj.Equals(null));
+            Assert.IsFalse(ReferenceEquals(obj, null));
+        }
+
+        [Test]
+        public void ReadOnlyObjectEqualsOther()
+        {
+            var obj1 = new ReadOnlyObject(new Object());
+            var obj2 = new ReadOnlyObject(new Object());
+
+            Assert.IsTrue(obj1 == obj2);
+            Assert.IsFalse(obj1 != obj2);
+            Assert.IsTrue(obj1.Equals(obj2));
+            Assert.IsFalse(ReferenceEquals(obj1, obj2));
+        }
+
+        [Test]
+        public void ReadOnlyObjectEqualsSame()
+        {
+            var obj1 = new ReadOnlyObject(new Object());
+            var obj2 = obj1;
+
+            Assert.IsTrue(obj1 == obj2);
+            Assert.IsFalse(obj1 != obj2);
+            Assert.IsTrue(obj1.Equals(obj2));
+            Assert.IsTrue(ReferenceEquals(obj1, obj2));
+        }
+
+        [Test]
+        public void ReadOnlyObjectEqualsSameObject()
+        {
+            var obj = new Object();
+            var obj1 = new ReadOnlyObject(obj);
+            var obj2 = new ReadOnlyObject(obj);
+
+            Assert.IsTrue(obj1 == obj2);
+            Assert.IsFalse(obj1 != obj2);
+            Assert.IsTrue(obj1.Equals(obj2));
+            Assert.IsFalse(ReferenceEquals(obj1, obj2));
+        }
+    }
+}
---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyObject.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyObject
+    {
+        HideFlags hideFlags { get; }
+        string name { get; }
+        int GetInstanceID();
+        string ToString();
+    }
+
+    public class ReadOnlyObject<T> : IReadOnlyObject where T : Object
+    {
+        protected readonly T _obj;
+
+        protected ReadOnlyObject(T obj)
+        {
+            if (ReferenceEquals(obj, null)) throw new ArgumentNullException(nameof(obj));
+
+            _obj = obj;
+        }
+
+        #region Properties
+
+        public HideFlags hideFlags => _obj.hideFlags;
+        public string name => _obj.name;
+
+        #endregion
+
+        #region Public Methods
+
+        public int GetInstanceID() => _obj.GetInstanceID();
+        public override string ToString() => _obj.ToString();
+
+        #endregion
+
+        #region Operators
+
+        public static implicit operator bool(ReadOnlyObject<T> self) => (ReferenceEquals(self, null) == false) && self._obj;
+
+        public static bool operator ==(ReadOnlyObject<T> lhs, ReadOnlyObject<T> rhs)
+        {
+            if (ReferenceEquals(lhs, null) && ReferenceEquals(rhs, null)) return true;
+            if (lhs && rhs) return (lhs._obj == rhs._obj);
+            if (lhs) return (lhs._obj == rhs);
+            if (rhs) return (lhs == rhs._obj);
+            return true;
+        }
+
+        public static bool operator !=(ReadOnlyObject<T> x, ReadOnlyObject<T> y) => !(x == y);
+
+        public override bool Equals(object other)
+        {
+            if (other == null) return _obj.Equals(other);
+            if (other is ReadOnlyObject<T> readOnlyOther) return (this == readOnlyOther);
+            if (other is Object obj) return (this == obj);
+
+            return false;
+        }
+
+        public override int GetHashCode() => _obj.GetHashCode();
+
+        #endregion
+    }
+
+    public class ReadOnlyObject : ReadOnlyObject<Object>
+    {
+        public ReadOnlyObject(Object obj) : base(obj)
+        {
+        }
+    }
+
+    public static class ObjectExtensions
+    {
+        public static ReadOnlyObject AsReadOnly(this Object self) => new ReadOnlyObject(self);
+    }
+}
