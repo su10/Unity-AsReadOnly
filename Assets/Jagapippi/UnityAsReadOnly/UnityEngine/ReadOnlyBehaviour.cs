7d324e24bd6b419263df8c19d13354498abf517f
su10 <1687366+su10@users.noreply.github.com>
2020-06-28 00:54:56 +0900

    ReadOnlyBehaviourクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyBehaviour : IReadOnlyComponent
+    {
+        bool enabled { get; }
+        bool isActiveAndEnabled { get; }
+    }
+
+    public class ReadOnlyBehaviour<T> : ReadOnlyComponent<T>, IReadOnlyBehaviour where T : Behaviour
+    {
+        protected ReadOnlyBehaviour(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public bool enabled => _obj.enabled;
+        public bool isActiveAndEnabled => _obj.isActiveAndEnabled;
+
+        #endregion
+    }
+
+    public class ReadOnlyBehaviour : ReadOnlyBehaviour<Behaviour>
+    {
+        public ReadOnlyBehaviour(Behaviour obj) : base(obj)
+        {
+        }
+    }
+
+    public static class BehaviourExtensions
+    {
+        public static ReadOnlyBehaviour AsReadOnly(this Behaviour self) => new ReadOnlyBehaviour(self);
+    }
+}
