5fba67198613713170745ffb7f47c7c6427c2912
su10 <1687366+su10@users.noreply.github.com>
2020-07-19 22:58:01 +0900

    ReadOnlyShaderクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyShader.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyShader : IReadOnlyObject
+    {
+        bool isSupported { get; }
+        int maximumLOD { get; }
+        int renderQueue { get; }
+    }
+
+    public sealed class ReadOnlyShader : ReadOnlyObject<Shader>, IReadOnlyShader
+    {
+        public ReadOnlyShader(Shader obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public bool isSupported => _obj.isSupported;
+        public int maximumLOD => _obj.maximumLOD;
+        public int renderQueue => _obj.renderQueue;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class ShaderExtensions
+    {
+        public static IReadOnlyShader AsReadOnly(this Shader self) => new ReadOnlyShader(self);
+    }
+}
