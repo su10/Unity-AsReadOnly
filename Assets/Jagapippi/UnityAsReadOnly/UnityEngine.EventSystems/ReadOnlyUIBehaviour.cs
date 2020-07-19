909d6c8c8991500c2d3c6f45492ed9426cebd17c
su10 <1687366+su10@users.noreply.github.com>
2020-07-19 23:58:07 +0900

    ReadOnlyUIBehaviourクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine.EventSystems/ReadOnlyUIBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.EventSystems;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyUIBehaviour : IReadOnlyMonoBehaviour
+    {
+        bool IsActive();
+        bool IsDestroyed();
+    }
+
+    public class ReadOnlyUIBehaviour<T> : ReadOnlyMonoBehaviour<T>, IReadOnlyUIBehaviour where T : UIBehaviour
+    {
+        protected ReadOnlyUIBehaviour(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+
+        public virtual bool IsActive() => _obj.IsActive();
+        public bool IsDestroyed() => _obj.IsDestroyed();
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyUIBehaviour : ReadOnlyUIBehaviour<UIBehaviour>
+    {
+        public ReadOnlyUIBehaviour(UIBehaviour obj) : base(obj)
+        {
+        }
+    }
+
+    public static class UIBehaviourExtensions
+    {
+        public static IReadOnlyUIBehaviour AsReadOnly(this UIBehaviour self) => new ReadOnlyUIBehaviour(self);
+    }
+}
