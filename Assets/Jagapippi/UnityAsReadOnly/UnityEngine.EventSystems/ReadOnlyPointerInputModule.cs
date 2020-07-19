c099c8151ff6568198dc83c30d1b18c3e25f0695
su10 <1687366+su10@users.noreply.github.com>
2020-07-20 00:35:07 +0900

    ReadOnlyPointerInputModuleクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine.EventSystems/ReadOnlyPointerInputModule.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine.EventSystems;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyPointerInputModule : IReadOnlyBaseInputModule
+    {
+        new bool IsPointerOverGameObject(int pointerId);
+        new string ToString();
+    }
+
+    public class ReadOnlyPointerInputModule<T> : ReadOnlyBaseInputModule<T>, IReadOnlyPointerInputModule where T : PointerInputModule
+    {
+        protected ReadOnlyPointerInputModule(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        #endregion
+
+        #region Public Methods
+
+        public new bool IsPointerOverGameObject(int pointerId) => _obj.IsPointerOverGameObject(pointerId);
+        public new string ToString() => _obj.ToString();
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyPointerInputModule : ReadOnlyPointerInputModule<PointerInputModule>
+    {
+        public ReadOnlyPointerInputModule(PointerInputModule obj) : base(obj)
+        {
+        }
+    }
+
+    public static class PointerInputModuleExtensions
+    {
+        public static IReadOnlyPointerInputModule AsReadOnly(this PointerInputModule self) => new ReadOnlyPointerInputModule(self);
+    }
+}
