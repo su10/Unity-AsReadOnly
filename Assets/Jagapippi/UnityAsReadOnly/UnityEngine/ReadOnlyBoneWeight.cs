74f6df25313262e5ab0ddf636221e69bd76c7554
su10 <1687366+su10@users.noreply.github.com>
2020-07-20 00:45:38 +0900

    ReadOnlyBoneWeight構造体追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyBoneWeight.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public readonly struct ReadOnlyBoneWeight
+    {
+        private readonly BoneWeight _boneWeight;
+
+        public ReadOnlyBoneWeight(BoneWeight boneWeight)
+        {
+            _boneWeight = boneWeight;
+        }
+
+        #region Properties
+
+        public float weight0 => _boneWeight.weight0;
+        public float weight1 => _boneWeight.weight1;
+        public float weight2 => _boneWeight.weight2;
+        public float weight3 => _boneWeight.weight3;
+        public int boneIndex0 => _boneWeight.boneIndex0;
+        public int boneIndex1 => _boneWeight.boneIndex1;
+        public int boneIndex2 => _boneWeight.boneIndex2;
+        public int boneIndex3 => _boneWeight.boneIndex3;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Operators
+
+        public static bool operator ==(ReadOnlyBoneWeight lhs, ReadOnlyBoneWeight rhs) => (lhs._boneWeight == rhs._boneWeight);
+        public static bool operator !=(ReadOnlyBoneWeight lhs, ReadOnlyBoneWeight rhs) => !(lhs == rhs);
+        public static bool operator ==(ReadOnlyBoneWeight lhs, BoneWeight rhs) => (lhs._boneWeight == rhs);
+        public static bool operator !=(ReadOnlyBoneWeight lhs, BoneWeight rhs) => !(lhs == rhs);
+        public static bool operator ==(BoneWeight lhs, ReadOnlyBoneWeight rhs) => (lhs == rhs._boneWeight);
+        public static bool operator !=(BoneWeight lhs, ReadOnlyBoneWeight rhs) => !(lhs == rhs);
+
+        public override bool Equals(object other)
+        {
+            if (other is BoneWeight boneWeight) return (_boneWeight == boneWeight);
+            if (other is ReadOnlyBoneWeight readOnlyBoneWeight) return (_boneWeight == readOnlyBoneWeight._boneWeight);
+            return false;
+        }
+
+        public override int GetHashCode() => _boneWeight.GetHashCode();
+
+        #endregion
+    }
+
+    public static class BoneWeightExtensions
+    {
+        public static ReadOnlyBoneWeight AsReadOnly(this BoneWeight self) => new ReadOnlyBoneWeight(self);
+    }
+}
