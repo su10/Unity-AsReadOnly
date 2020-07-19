f4e2758244225dbc128a604c5d7122dd188dd450
su10 <1687366+su10@users.noreply.github.com>
2020-07-19 23:46:31 +0900

    ReadOnlyAudioMixerSnapshotクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine.Audio/ReadOnlyAudioMixerSnapshot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.Audio;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyAudioMixerSnapshot : IReadOnlyObject
+    {
+        IReadOnlyAudioMixer audioMixer { get; }
+        // void TransitionTo(float timeToReach);
+    }
+
+    public sealed class ReadOnlyAudioMixerSnapshot : ReadOnlyObject<AudioMixerSnapshot>, IReadOnlyAudioMixerSnapshot
+    {
+        public ReadOnlyAudioMixerSnapshot(AudioMixerSnapshot obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public IReadOnlyAudioMixer audioMixer => (_obj.audioMixer == null) ? null : _obj.audioMixer.AsReadOnly();
+
+        #endregion
+
+        #region Public Methods
+
+        // public void TransitionTo(float timeToReach) => _obj.TransitionTo(timeToReach);
+
+        #endregion
+    }
+
+    public static class AudioMixerSnapshotExtensions
+    {
+        public static IReadOnlyAudioMixerSnapshot AsReadOnly(this AudioMixerSnapshot self) => new ReadOnlyAudioMixerSnapshot(self);
+    }
+}
