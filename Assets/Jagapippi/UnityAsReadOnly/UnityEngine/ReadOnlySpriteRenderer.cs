58814162e8ef06d28d1c97afab9d82afcfdf20b6
su10 <1687366+su10@users.noreply.github.com>
2020-07-19 23:21:50 +0900

    ReadOnlySpriteRendererクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlySpriteRenderer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlySpriteRenderer : IReadOnlyRenderer
+    {
+        float adaptiveModeThreshold { get; }
+        Color color { get; }
+        SpriteDrawMode drawMode { get; }
+        bool flipX { get; }
+        bool flipY { get; }
+        SpriteMaskInteraction maskInteraction { get; }
+        Vector2 size { get; }
+        Sprite sprite { get; }
+        SpriteSortPoint spriteSortPoint { get; }
+        SpriteTileMode tileMode { get; }
+    }
+
+    public sealed class ReadOnlySpriteRenderer : ReadOnlyRenderer<SpriteRenderer>, IReadOnlySpriteRenderer
+    {
+        public ReadOnlySpriteRenderer(SpriteRenderer obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public float adaptiveModeThreshold => _obj.adaptiveModeThreshold;
+        public Color color => _obj.color;
+        public SpriteDrawMode drawMode => _obj.drawMode;
+        public bool flipX => _obj.flipX;
+        public bool flipY => _obj.flipY;
+        public SpriteMaskInteraction maskInteraction => _obj.maskInteraction;
+        public Vector2 size => _obj.size;
+        public Sprite sprite => _obj.sprite;
+        public SpriteSortPoint spriteSortPoint => _obj.spriteSortPoint;
+        public SpriteTileMode tileMode => _obj.tileMode;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class SpriteRendererExtensions
+    {
+        public static IReadOnlySpriteRenderer AsReadOnly(this SpriteRenderer self) => new ReadOnlySpriteRenderer(self);
+    }
+}
