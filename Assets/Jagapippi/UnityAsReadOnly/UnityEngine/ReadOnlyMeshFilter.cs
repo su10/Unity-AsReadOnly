3e73c62deeea3b9a0a3414f4c6b6be1baa420320
su10 <1687366+su10@users.noreply.github.com>
2020-07-24 00:32:42 +0900

    ReadOnlyMeshFilterクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyMeshFilter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyMeshFilter
+    {
+        IReadOnlyMesh mesh { get; }
+        IReadOnlyMesh sharedMesh { get; }
+    }
+
+    public sealed class ReadOnlyMeshFilter : ReadOnlyComponent<MeshFilter>, IReadOnlyMeshFilter
+    {
+        public ReadOnlyMeshFilter(MeshFilter obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public ReadOnlyMesh mesh => _obj.mesh.IsTrulyNull() ? null : _obj.mesh.AsReadOnly();
+        IReadOnlyMesh IReadOnlyMeshFilter.mesh => this.mesh;
+        public ReadOnlyMesh sharedMesh => _obj.sharedMesh.IsTrulyNull() ? null : _obj.sharedMesh.AsReadOnly();
+        IReadOnlyMesh IReadOnlyMeshFilter.sharedMesh => this.sharedMesh;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class MeshFilterExtensions
+    {
+        public static ReadOnlyMeshFilter AsReadOnly(this MeshFilter self) => new ReadOnlyMeshFilter(self);
+    }
+}
