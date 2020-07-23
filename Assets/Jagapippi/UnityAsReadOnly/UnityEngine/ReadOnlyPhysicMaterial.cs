94b0be8fde109ecca682dfd8bcd755aeebc583dd
su10 <1687366+su10@users.noreply.github.com>
2020-07-24 00:49:42 +0900

    ReadOnlyPhysicMaterialクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyPhysicMaterial.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyPhysicMaterial
+    {
+        PhysicMaterialCombine bounceCombine { get; }
+        float bounciness { get; }
+        float dynamicFriction { get; }
+        PhysicMaterialCombine frictionCombine { get; }
+        float staticFriction { get; }
+    }
+
+    public class ReadOnlyPhysicMaterial : ReadOnlyObject<PhysicMaterial>, IReadOnlyPhysicMaterial
+    {
+        public ReadOnlyPhysicMaterial(PhysicMaterial obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public PhysicMaterialCombine bounceCombine => _obj.bounceCombine;
+        public float bounciness => _obj.bounciness;
+        public float dynamicFriction => _obj.dynamicFriction;
+        public PhysicMaterialCombine frictionCombine => _obj.frictionCombine;
+        public float staticFriction => _obj.staticFriction;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class PhysicMaterialExtensions
+    {
+        public static ReadOnlyPhysicMaterial AsReadOnly(this PhysicMaterial self) => new ReadOnlyPhysicMaterial(self);
+    }
+}
