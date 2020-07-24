828bb4081bb32e11c34d0b76496e3ef7cd25eda1
su10 <1687366+su10@users.noreply.github.com>
2020-07-24 12:01:44 +0900

    ReadOnlySphereColliderクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlySphereCollider.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlySphereCollider
+    {
+        Vector3 center { get; }
+        float radius { get; }
+    }
+
+    public class ReadOnlySphereCollider<T> : ReadOnlyCollider<T>, IReadOnlySphereCollider where T : SphereCollider
+    {
+        protected ReadOnlySphereCollider(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public Vector3 center => _obj.center;
+        public float radius => _obj.radius;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public sealed class ReadOnlySphereCollider : ReadOnlySphereCollider<SphereCollider>
+    {
+        public ReadOnlySphereCollider(SphereCollider obj) : base(obj)
+        {
+        }
+    }
+
+    public static class SphereColliderExtensions
+    {
+        public static ReadOnlySphereCollider AsReadOnly(this SphereCollider self) => new ReadOnlySphereCollider(self);
+    }
+}
