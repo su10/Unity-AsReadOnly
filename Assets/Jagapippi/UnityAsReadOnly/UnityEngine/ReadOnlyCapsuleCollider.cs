183dffbdc4e7495487b43c098bf999b47cd07559
su10 <1687366+su10@users.noreply.github.com>
2020-07-24 12:02:48 +0900

    ReadOnlyCapsuleColliderクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyCapsuleCollider.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyCapsuleCollider
+    {
+        Vector3 center { get; }
+        int direction { get; }
+        float height { get; }
+        float radius { get; }
+    }
+
+    public class ReadOnlyCapsuleCollider<T> : ReadOnlyCollider<T>, IReadOnlyCapsuleCollider where T : CapsuleCollider
+    {
+        protected ReadOnlyCapsuleCollider(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public Vector3 center => _obj.center;
+        public int direction => _obj.direction;
+        public float height => _obj.height;
+        public float radius => _obj.radius;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyCapsuleCollider : ReadOnlyCapsuleCollider<CapsuleCollider>
+    {
+        public ReadOnlyCapsuleCollider(CapsuleCollider obj) : base(obj)
+        {
+        }
+    }
+
+    public static class CapsuleColliderExtensions
+    {
+        public static ReadOnlyCapsuleCollider AsReadOnly(this CapsuleCollider self) => new ReadOnlyCapsuleCollider(self);
+    }
+}
