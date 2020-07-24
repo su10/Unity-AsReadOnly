b47ddf054173369617c5bddd10ff2e68bf45b40f
su10 <1687366+su10@users.noreply.github.com>
2020-07-24 20:56:48 +0900

    ReadOnlyPhysicsMaterial2Dクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyPhysicsMaterial2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyPhysicsMaterial2D
+    {
+        float bounciness { get; }
+        float friction { get; }
+    }
+
+    public sealed class ReadOnlyPhysicsMaterial2D : ReadOnlyObject<PhysicsMaterial2D>, IReadOnlyPhysicsMaterial2D
+    {
+        public ReadOnlyPhysicsMaterial2D(PhysicsMaterial2D obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public float bounciness => _obj.bounciness;
+        public float friction => _obj.friction;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class PhysicsMaterial2DExtensions
+    {
+        public static ReadOnlyPhysicsMaterial2D AsReadOnly(this PhysicsMaterial2D self) => new ReadOnlyPhysicsMaterial2D(self);
+    }
+}
