eebff6c02acbb6871451341edf46459fb8f09778
su10 <1687366+su10@users.noreply.github.com>
2020-07-24 14:07:30 +0900

    ReadOnlyMeshColliderクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyMeshCollider.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyMeshCollider
+    {
+        bool convex { get; }
+        MeshColliderCookingOptions cookingOptions { get; }
+        IReadOnlyMesh sharedMesh { get; }
+    }
+
+    public class ReadOnlyMeshCollider<T> : ReadOnlyCollider<T>, IReadOnlyMeshCollider where T : MeshCollider
+    {
+        protected ReadOnlyMeshCollider(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public bool convex => _obj.convex;
+        public MeshColliderCookingOptions cookingOptions => _obj.cookingOptions;
+        public ReadOnlyMesh sharedMesh => _obj.sharedMesh.AsReadOnly();
+        IReadOnlyMesh IReadOnlyMeshCollider.sharedMesh => this.sharedMesh;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyMeshCollider : ReadOnlyMeshCollider<MeshCollider>
+    {
+        public ReadOnlyMeshCollider(MeshCollider obj) : base(obj)
+        {
+        }
+    }
+
+    public static class MeshColliderExtensions
+    {
+        public static ReadOnlyMeshCollider AsReadOnly(this MeshCollider self) => new ReadOnlyMeshCollider(self);
+    }
+}
