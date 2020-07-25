daafa9d3bb3efb7454957344cfb6f21c871ead6d
su10 <1687366+su10@users.noreply.github.com>
2020-07-25 12:20:39 +0900

    ReadOnlyRuntimeAnimatorControllerクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyRuntimeAnimatorController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyRuntimeAnimatorController
+    {
+        IReadOnlyAnimationClip[] animationClips { get; }
+    }
+
+    public class ReadOnlyRuntimeAnimatorController<T> : ReadOnlyObject<T>, IReadOnlyRuntimeAnimatorController where T : RuntimeAnimatorController
+    {
+        protected ReadOnlyRuntimeAnimatorController(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public ReadOnlyAnimationClip[] animationClips => _obj.animationClips.Select(a => a.AsReadOnly()).ToArray();
+        IReadOnlyAnimationClip[] IReadOnlyRuntimeAnimatorController.animationClips => this.animationClips;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyRuntimeAnimatorController : ReadOnlyRuntimeAnimatorController<RuntimeAnimatorController>
+    {
+        public ReadOnlyRuntimeAnimatorController(RuntimeAnimatorController obj) : base(obj)
+        {
+        }
+    }
+
+    public static class RuntimeAnimatorControllerExtensions
+    {
+        public static ReadOnlyRuntimeAnimatorController AsReadOnly(this RuntimeAnimatorController self) => new ReadOnlyRuntimeAnimatorController(self);
+    }
+}
