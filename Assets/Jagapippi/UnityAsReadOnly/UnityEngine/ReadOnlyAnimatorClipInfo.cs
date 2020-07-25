272dbfc7d039e87a56493b1cd0575b84c0e0b876
su10 <1687366+su10@users.noreply.github.com>
2020-07-25 12:28:51 +0900

    ReadOnlyAnimatorClipInfo構造体追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyAnimatorClipInfo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public readonly struct ReadOnlyAnimatorClipInfo
+    {
+        private readonly AnimatorClipInfo _info;
+
+        public ReadOnlyAnimatorClipInfo(AnimatorClipInfo info)
+        {
+            _info = info;
+        }
+
+        #region Properties
+
+        public ReadOnlyAnimationClip clip => _info.clip.AsReadOnly();
+        public float weight => _info.weight;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class AnimatorClipInfoExtensions
+    {
+        public static ReadOnlyAnimatorClipInfo AsReadOnly(this AnimatorClipInfo self) => new ReadOnlyAnimatorClipInfo(self);
+    }
+}
