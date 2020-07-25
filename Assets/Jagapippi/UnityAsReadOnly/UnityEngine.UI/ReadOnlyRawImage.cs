90e489d943a07035829eb4248541226aa205f8f6
su10 <1687366+su10@users.noreply.github.com>
2020-07-25 11:55:25 +0900

    ReadOnlyRawImageクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine.UI/ReadOnlyRawImage.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyRawImage
+    {
+        Texture mainTexture { get; }
+        Texture texture { get; }
+        Rect uvRect { get; }
+        // void SetNativeSize();
+    }
+
+    public class ReadOnlyRawImage<T> : ReadOnlyMaskableGraphic<T>, IReadOnlyRawImage where T : RawImage
+    {
+        protected ReadOnlyRawImage(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public override Texture mainTexture => _obj.mainTexture;
+        public Texture texture => _obj.texture;
+        public Rect uvRect => _obj.uvRect;
+
+        #endregion
+
+        #region Public Methods
+
+        // public void SetNativeSize() => _obj.SetNativeSize();
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyRawImage : ReadOnlyRawImage<RawImage>
+    {
+        public ReadOnlyRawImage(RawImage obj) : base(obj)
+        {
+        }
+    }
+
+    public static class RawImageExtensions
+    {
+        public static ReadOnlyRawImage AsReadOnly(this RawImage self) => new ReadOnlyRawImage(self);
+    }
+}
