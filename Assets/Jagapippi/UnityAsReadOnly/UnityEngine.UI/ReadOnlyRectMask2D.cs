f0998850b5043f54fad64062030e8749bd9c1af2
su10 <1687366+su10@users.noreply.github.com>
2020-07-25 16:52:36 +0900

    ReadOnlyRectMask2Dクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine.UI/ReadOnlyRectMask2D.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyRectMask2D
+    {
+        Rect canvasRect { get; }
+        IReadOnlyRectTransform rectTransform { get; }
+        // void AddClippable(IClippable clippable);
+        bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera);
+        // void PerformClipping();
+        // void RemoveClippable(IClippable clippable);
+    }
+
+    public class ReadOnlyRectMask2D<T> : ReadOnlyUIBehaviour<T>, IReadOnlyRectMask2D where T : RectMask2D
+    {
+        protected ReadOnlyRectMask2D(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public Rect canvasRect => _obj.canvasRect;
+        public ReadOnlyRectTransform rectTransform => _obj.rectTransform.IsTrulyNull() ? null : _obj.rectTransform.AsReadOnly();
+        IReadOnlyRectTransform IReadOnlyRectMask2D.rectTransform => this.rectTransform;
+
+        #endregion
+
+        #region Public Methods
+
+        // public void AddClippable(IClippable clippable) => _obj.AddClippable(clippable);
+        public virtual bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera) => _obj.IsRaycastLocationValid(sp, eventCamera);
+        // public void PerformClipping() => _obj.PerformClipping();
+        // public void RemoveClippable(IClippable clippable) => _obj.RemoveClippable(clippable);
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyRectMask2D : ReadOnlyRectMask2D<RectMask2D>
+    {
+        public ReadOnlyRectMask2D(RectMask2D obj) : base(obj)
+        {
+        }
+    }
+
+    public static class RectMask2DExtensions
+    {
+        public static ReadOnlyRectMask2D AsReadOnly(this RectMask2D self) => new ReadOnlyRectMask2D(self);
+    }
+}
