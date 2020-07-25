2bf650e9b1ddf76c26003039eb0736c5b6c1cb21
su10 <1687366+su10@users.noreply.github.com>
2020-07-26 03:26:43 +0900

    ReadOnlyCircleCollider2Dクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyCircleCollider2D.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyCircleCollider2D
+    {
+        float radius { get; }
+    }
+
+    public sealed class ReadOnlyCircleCollider2D : ReadOnlyCollider2D<CircleCollider2D>, IReadOnlyCircleCollider2D
+    {
+        public ReadOnlyCircleCollider2D(CircleCollider2D obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public float radius => _obj.radius;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class CircleCollider2DExtensions
+    {
+        public static ReadOnlyCircleCollider2D AsReadOnly(this CircleCollider2D self) => self.IsTrulyNull() ? null : new ReadOnlyCircleCollider2D(self);
+    }
+}
