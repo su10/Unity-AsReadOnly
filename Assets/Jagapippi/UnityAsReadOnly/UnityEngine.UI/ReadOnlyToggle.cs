a4f59e3fcd48b17296393f0ce018ecbba4d58ecc
su10 <1687366+su10@users.noreply.github.com>
2020-07-25 16:32:30 +0900

    ReadOnlyToggleクラスとReadOnlyToggleGroupクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine.UI/ReadOnlyToggle.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.UI;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyToggle
+    {
+        IReadOnlyToggleGroup group { get; }
+        bool isOn { get; }
+        // void GraphicUpdateComplete();
+        // void LayoutComplete();
+        // void OnPointerClick(PointerEventData eventData);
+        // void OnSubmit(BaseEventData eventData);
+        // void Rebuild(CanvasUpdate executing);
+    }
+
+    public class ReadOnlyToggle<T> : ReadOnlySelectable<T>, IReadOnlyToggle where T : Toggle
+    {
+        protected ReadOnlyToggle(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public ReadOnlyToggleGroup group => _obj.group.IsTrulyNull() ? null : _obj.group.AsReadOnly();
+        IReadOnlyToggleGroup IReadOnlyToggle.group => this.group;
+        public bool isOn => _obj.isOn;
+
+        #endregion
+
+        #region Public Methods
+
+        // public void GraphicUpdateComplete() => _obj.GraphicUpdateComplete();
+        // public void LayoutComplete() => _obj.LayoutComplete();
+        // public void OnPointerClick(PointerEventData eventData) => _obj.OnPointerClick(eventData);
+        // public void OnSubmit(BaseEventData eventData) => _obj.OnSubmit(eventData);
+        // public void Rebuild(CanvasUpdate executing) => _obj.Rebuild(executing);
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyToggle : ReadOnlyToggle<Toggle>
+    {
+        public ReadOnlyToggle(Toggle obj) : base(obj)
+        {
+        }
+    }
+
+    public static class ToggleExtensions
+    {
+        public static ReadOnlyToggle AsReadOnly(this Toggle self) => new ReadOnlyToggle(self);
+    }
+}
---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine.UI/ReadOnlyToggleGroup.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyToggleGroup
+    {
+        bool allowSwitchOff { get; }
+        IEnumerable<IReadOnlyToggle> ActiveToggles();
+        bool AnyTogglesOn();
+        // void NotifyToggleOn(Toggle toggle);
+        // void RegisterToggle(Toggle toggle);
+        // void SetAllTogglesOff();
+        // void UnregisterToggle(Toggle toggle);
+    }
+
+    public class ReadOnlyToggleGroup<T> : ReadOnlyUIBehaviour<T>, IReadOnlyToggleGroup where T : ToggleGroup
+    {
+        protected ReadOnlyToggleGroup(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public bool allowSwitchOff => _obj.allowSwitchOff;
+
+        #endregion
+
+        #region Public Methods
+
+        public IEnumerable<ReadOnlyToggle> ActiveToggles() => _obj.ActiveToggles().Select(t => t.AsReadOnly());
+        IEnumerable<IReadOnlyToggle> IReadOnlyToggleGroup.ActiveToggles() => this.ActiveToggles();
+        public bool AnyTogglesOn() => _obj.AnyTogglesOn();
+        // public void NotifyToggleOn(Toggle toggle) => _obj.NotifyToggleOn(toggle);
+        // public void RegisterToggle(Toggle toggle) => _obj.RegisterToggle(toggle);
+        // public void SetAllTogglesOff() => _obj.SetAllTogglesOff();
+        // public void UnregisterToggle(Toggle toggle) => _obj.UnregisterToggle(toggle);
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyToggleGroup : ReadOnlyToggleGroup<ToggleGroup>
+    {
+        public ReadOnlyToggleGroup(ToggleGroup obj) : base(obj)
+        {
+        }
+    }
+
+    public static class ToggleGroupExtensions
+    {
+        public static ReadOnlyToggleGroup AsReadOnly(this ToggleGroup self) => new ReadOnlyToggleGroup(self);
+    }
+}
