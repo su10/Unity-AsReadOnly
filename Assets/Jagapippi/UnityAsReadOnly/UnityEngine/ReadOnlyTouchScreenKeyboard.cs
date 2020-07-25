8e462af33a1981fab460a737590a8f3a00721eb0
su10 <1687366+su10@users.noreply.github.com>
2020-07-25 17:40:45 +0900

    ReadOnlyTouchScreenKeyboardクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyTouchScreenKeyboard.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyTouchScreenKeyboard
+    {
+        bool active { get; }
+        bool canGetSelection { get; }
+        bool canSetSelection { get; }
+        int characterLimit { get; }
+        RangeInt selection { get; }
+        TouchScreenKeyboard.Status status { get; }
+        int targetDisplay { get; }
+        string text { get; }
+        TouchScreenKeyboardType type { get; }
+    }
+
+    public class ReadOnlyTouchScreenKeyboard<T> : IReadOnlyTouchScreenKeyboard where T : TouchScreenKeyboard
+    {
+        protected readonly TouchScreenKeyboard _obj;
+
+        public ReadOnlyTouchScreenKeyboard(T obj)
+        {
+            _obj = obj;
+        }
+
+        #region Properties
+
+        public bool active => _obj.active;
+        public bool canGetSelection => _obj.canGetSelection;
+        public bool canSetSelection => _obj.canSetSelection;
+        public int characterLimit => _obj.characterLimit;
+        public RangeInt selection => _obj.selection;
+        public TouchScreenKeyboard.Status status => _obj.status;
+        public int targetDisplay => _obj.targetDisplay;
+        public string text => _obj.text;
+        public TouchScreenKeyboardType type => _obj.type;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyTouchScreenKeyboard : ReadOnlyTouchScreenKeyboard<TouchScreenKeyboard>
+    {
+        public ReadOnlyTouchScreenKeyboard(TouchScreenKeyboard obj) : base(obj)
+        {
+        }
+    }
+
+    public static class TouchScreenKeyboardExtensions
+    {
+        public static ReadOnlyTouchScreenKeyboard AsReadOnly(this TouchScreenKeyboard self) => new ReadOnlyTouchScreenKeyboard(self);
+    }
+}
