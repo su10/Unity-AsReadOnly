183d103f1968b39d1b27b854ed5966f98aaf45cb
su10 <1687366+su10@users.noreply.github.com>
2020-07-25 16:25:08 +0900

    ReadOnlyButtonクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine.UI/ReadOnlyButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.UI;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyButton
+    {
+        // Button.ButtonClickedEvent onClick { get; }
+        // void OnPointerClick(PointerEventData eventData);
+        // void OnSubmit(BaseEventData eventData);
+    }
+
+    public class ReadOnlyButton<T> : ReadOnlySelectable<T>, IReadOnlyButton where T : Button
+    {
+        protected ReadOnlyButton(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        // public Button.ButtonClickedEvent onClick => _obj.onClick;
+
+        #endregion
+
+        #region Public Methods
+
+        // public void OnPointerClick(PointerEventData eventData) => _obj.OnPointerClick(eventData);
+        // public void OnSubmit(BaseEventData eventData) => _obj.OnSubmit(eventData);
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyButton : ReadOnlyButton<Button>
+    {
+        public ReadOnlyButton(Button obj) : base(obj)
+        {
+        }
+    }
+
+    public static class ButtonExtensions
+    {
+        public static ReadOnlyButton AsReadOnly(this Button self) => new ReadOnlyButton(self);
+    }
+}
