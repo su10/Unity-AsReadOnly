72077704834fe65916657e330025612c0e0637b9
su10 <1687366+su10@users.noreply.github.com>
2020-07-26 03:25:41 +0900

    ReadOnlyCapsuleCollider2Dクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyCapsuleCollider2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyCapsuleCollider2D
+    {
+        CapsuleDirection2D direction { get; }
+        Vector2 size { get; }
+    }
+
+    public sealed class ReadOnlyCapsuleCollider2D : ReadOnlyCollider2D<CapsuleCollider2D>, IReadOnlyCapsuleCollider2D
+    {
+        public ReadOnlyCapsuleCollider2D(CapsuleCollider2D obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public CapsuleDirection2D direction => _obj.direction;
+        public Vector2 size => _obj.size;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class CapsuleCollider2DExtensions
+    {
+        public static ReadOnlyCapsuleCollider2D AsReadOnly(this CapsuleCollider2D self) => self.IsTrulyNull() ? null : new ReadOnlyCapsuleCollider2D(self);
+    }
+}
