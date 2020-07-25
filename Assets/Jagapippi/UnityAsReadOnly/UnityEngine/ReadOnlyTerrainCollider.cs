0307566e65baa1e83c594fbdc02cf92edb299f47
su10 <1687366+su10@users.noreply.github.com>
2020-07-26 03:20:36 +0900

    ReadOnlyTerrainColliderクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyTerrainCollider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyTerrainCollider
+    {
+        IReadOnlyTerrainData terrainData { get; }
+    }
+
+    public class ReadOnlyTerrainCollider<T> : ReadOnlyCollider<T>, IReadOnlyTerrainCollider where T : TerrainCollider
+    {
+        protected ReadOnlyTerrainCollider(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public ReadOnlyTerrainData terrainData => _obj.terrainData.AsReadOnly();
+        IReadOnlyTerrainData IReadOnlyTerrainCollider.terrainData => this.terrainData;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyTerrainCollider : ReadOnlyTerrainCollider<TerrainCollider>
+    {
+        public ReadOnlyTerrainCollider(TerrainCollider obj) : base(obj)
+        {
+        }
+    }
+
+    public static class TerrainColliderExtensions
+    {
+        public static ReadOnlyTerrainCollider AsReadOnly(this TerrainCollider self) => self.IsTrulyNull() ? null : new ReadOnlyTerrainCollider(self);
+    }
+}
