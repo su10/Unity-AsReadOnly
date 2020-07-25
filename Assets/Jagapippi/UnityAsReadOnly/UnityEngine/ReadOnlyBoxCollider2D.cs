7a27225bd58cd9efd2be8a71da26f4ff33458c84
su10 <1687366+su10@users.noreply.github.com>
2020-07-26 03:23:56 +0900

    ReadOnlyBoxCollider2Dクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyBoxCollider2D.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyBoxCollider2D
+    {
+        bool autoTiling { get; }
+        float edgeRadius { get; }
+        Vector2 size { get; }
+    }
+
+    public sealed class ReadOnlyBoxCollider2D : ReadOnlyCollider2D<BoxCollider2D>, IReadOnlyBoxCollider2D
+    {
+        public ReadOnlyBoxCollider2D(BoxCollider2D obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public bool autoTiling => _obj.autoTiling;
+        public float edgeRadius => _obj.edgeRadius;
+        public Vector2 size => _obj.size;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public static class BoxCollider2DExtensions
+    {
+        public static ReadOnlyBoxCollider2D AsReadOnly(this BoxCollider2D self) => self.IsTrulyNull() ? null : new ReadOnlyBoxCollider2D(self);
+    }
+}
