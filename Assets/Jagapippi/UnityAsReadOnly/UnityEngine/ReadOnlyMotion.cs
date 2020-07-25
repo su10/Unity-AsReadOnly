9df32dd7c7e2348e7d800a1a0cfdb7c48f896d93
su10 <1687366+su10@users.noreply.github.com>
2020-07-25 12:08:57 +0900

    ReadOnlyMotionクラス追加

---
+++ Assets/Jagapippi/UnityAsReadOnly/UnityEngine/ReadOnlyMotion.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Jagapippi.UnityAsReadOnly
+{
+    public interface IReadOnlyMotion
+    {
+        float apparentSpeed { get; }
+        float averageAngularSpeed { get; }
+        float averageDuration { get; }
+        Vector3 averageSpeed { get; }
+        bool isHumanMotion { get; }
+        bool isLooping { get; }
+        bool legacy { get; }
+    }
+
+    public class ReadOnlyMotion<T> : ReadOnlyObject<T>, IReadOnlyMotion where T : Motion
+    {
+        protected ReadOnlyMotion(T obj) : base(obj)
+        {
+        }
+
+        #region Properties
+
+        public float apparentSpeed => _obj.apparentSpeed;
+        public float averageAngularSpeed => _obj.averageAngularSpeed;
+        public float averageDuration => _obj.averageDuration;
+        public Vector3 averageSpeed => _obj.averageSpeed;
+        public bool isHumanMotion => _obj.isHumanMotion;
+        public bool isLooping => _obj.isLooping;
+        public bool legacy => _obj.legacy;
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+
+    public sealed class ReadOnlyMotion : ReadOnlyMotion<Motion>
+    {
+        public ReadOnlyMotion(Motion obj) : base(obj)
+        {
+        }
+    }
+
+    public static class MotionExtensions
+    {
+        public static ReadOnlyMotion AsReadOnly(this Motion self) => new ReadOnlyMotion(self);
+    }
+}
